d3a7e9f7d7e46b722d3bf70e2e48925cc43b80f7
ylesueur-dawan <ylesueur@dawan.fr>
2021-03-22 14:53:56 +0100

    Exercices

---
+++ 99-exercices/js/ex1.js
@@ -0,0 +1,79 @@
+/*
+ * Déclarer une variable 'nombre' qui contiendra un nombre entier, et lui affecter la valeur 13. 
+ */
+/*
+* Déclarer une variable nombre2 qui, contiendra le nombre flottant/réel/virgule:  15,5
+*/
+
+
+/* Déclarer une variable 'b', qui prendra la valeur : true */
+
+
+/* Déclarer une variable 'langage', qui prendra la valeur : 'Javascript' */
+
+/*
+* Déclarer une variable nombre3 qui ne pourra plus changer de valeur après lui avoir
+* donner la valeur 60.
+*/
+
+/*
+* Déclarer deux variables qui s'appellent nb et nb2 qui auront pour valeur 7 et 6
+*/
+
+
+
+/* afficher le message 'hello world' si nombre est supérieur ou égal à 4 */
+
+/*
+* Réaliser un switch qui prend nb2 et qui affiche: 
+*        - "a" si nombre2 est égal à 3 
+*        - "b" si nombre2 est égal à 5 
+*        - "c" si nombre2 est égal à 7 
+*        - "d" si nombre2 est égal à autre chose.
+*/
+
+
+//  demander à l'utilisateur de saisir un 1er nombre
+//  demander à l'utilisateur de saisir un 2ème nombre
+//  additionner les nombres et afficher le résultat
+
+// TP: Convertir des pouces en centimetres
+//  demander à l'utilisateur de saisir des pouces
+//  1" (1 pouce) = 2,54 centimètres 
+//  https://www.pouce-cm.fr/
+
+
+// TP: Convertir des heures en minutes
+//  demander à l'utilisateur de saisir des heures
+//  demander à l'utilisateur de saisir des minutes
+//  convertir les heures & minutes en minutes
+
+
+// TP: Convertir des degres Celcius en degrès Fahrenheit
+//  https://www.metric-conversions.org/fr/temperature/fahrenheit-en-celsius.htm
+//  demander à l'utilisateur de saisir des degres celcius
+//  convertir les °C en °F
+
+
+//  TP
+//  Afficher un message à l'utilisateur pour savoir s'il souhaite
+//      1- Convertir des pouces en centimètres
+//      2- Convertir des centimètres en pouces 
+//      3- Quitter
+//  Si l'utilisateur saisit 1:
+//       * Demander a l'utilisateur de saisir un nombre: "Nombre de pouces: "
+//       * on convertit
+//       * on affiche le resultat
+//  Si l'utilisateur saisit 2:
+//       * Demander a l'utilisateur de saisir un nombre: "Nombre de centimètre: "
+//       * on convertit
+//       * on affiche le resultat
+// Sinon on affiche:
+//       *Je n'ai pas compris
+
+
+
+// Bonus:
+// En partant d'une heure et de minutes données
+// ex: heure = 2 et minutes = 45
+// Si on ajoute 30 minutes, au minutes, on obtient 3h15
